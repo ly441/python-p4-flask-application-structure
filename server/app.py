bc38b3638d6082fefd2357015b61c6eaae7773d3
Lynn <koliilynn@gmail.com>
2025-06-14 15:53:39 +0300

    refactor app.py to enhance pet directory functionality and improve response handling

--- server/app.py
+++ server/app.py
@@ -1,15 +1,53 @@
+from flask import Flask, make_response
+from flask_migrate import Migrate
 
-from flask import Flask
+from models import db, Pet
 
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///app.db'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
+
+migrate = Migrate(app, db)
+
+db.init_app(app)
+
 
 @app.route('/')
 def index():
-    return '<h1>Welcome to my page!</h1>'
+    response = make_response(
+        '<h1>Welcome to the pet directory!</h1>',
+        200
+    )
+    return response
 
-@app.route('/<string:username>')
-def user(username):
-    return f'<h1>Profile for {username}</h1>'
+
+@app.route('/pets/<int:id>')
+def pet_by_id(id):
+    pet = Pet.query.filter(Pet.id == id).first()
+
+    if pet:
+        response_body = f'<p>{pet.name} {pet.species}</p>'
+        response_status = 200
+    else:
+        response_body = f'<p>Pet {id} not found</p>'
+        response_status = 404
+
+    response = make_response(response_body, response_status)
+    return response
+
+
+@app.route('/species/<string:species>')
+def pet_by_species(species):
+    pets = Pet.query.filter_by(species=species).all()
+
+    size = len(pets)  # all() returns a list so we can get length
+    response_body = f'<h2>There are {size} {species}s</h2>'
+    for pet in pets:
+        response_body += f'<p>{pet.name}</p>'
+    response = make_response(response_body, 200)
+    return response
+
 
 if __name__ == '__main__':
     app.run(port=5555, debug=True)
+
